0ee32d69c343bdd8125b22acb109d234276d5b34
Zachary Stucke <zstucke@e1z1r3p46.42.us.org>
2018-11-08 22:50:38 -0800

    Everything on my mac as of today

---
+++ compile.sh
@@ -0,0 +1,3 @@
+gcc -Wextra -Wall -Werror -g $1 -L/nfs/2018/z/zstucke -lft
+./a.out $2 $3 $4 $5 $6
+rm a.out
---
+++ headers.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   headers.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zstucke <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/01 16:14:23 by zstucke           #+#    #+#             */
+/*   Updated: 2018/11/01 16:46:36 by zstucke          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEADERS_H
+# define HEADERS_H
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+void	ft_putnbr(int x);
+int		ft_atoi(char *str);
+int		ft_strlen(char *str);
+char 	*ft_strcpy(char *dest, char *src);
+char	*ft_strstr(char *str, char *to_find);
+#include <stdio.h>
+
+#endif
